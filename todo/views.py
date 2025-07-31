196b56b4da4b9703b8e3d45fa8192ac7cf72b225
Navdeep <navdeepjaj016@gmail.com>
2025-07-31 15:28:10 +0530

    None much

--- todo/views.py
+++ todo/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login,logout,authenticate
 from .forms import TodoForm
+from .models import Todo
 
 
 def home(request):
-	return render(request,'todo/home.html')
+	return render(request,'todo/currentprojects.html')
 
 
 def signupuser(request):
@@ -29,7 +30,8 @@ def signupuser(request):
 
 
 def currenttodos(request):
-	return render(request,'todo/currenttodos.html')
+	todos = Todo.objects.filter(user = request.user,datecompleted__isnull=True)
+	return render(request,'todo/currenttodos.html',{'todos':todos})
 
 
 def logoutuser(request):
